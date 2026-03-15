dae6d1de63eaf3163f9328de0a697ac5bcf25026
chivorotkiv <git@chiv.info>
2026-03-15 16:55:29 +0000

    evj-hello v0.0.0-2

--- Casks/evj-hello.rb
+++ Casks/evj-hello.rb
@@ -1,6 +1,6 @@
 cask "evj-hello" do
-  version "0.0.0-1"
-  sha256 "2484a1a3b294e13982a651b63f8bf355af86bf6f9f09ae8e42a8b52e472a411f"
+  version "0.0.0-2"
+  sha256 "f2a0817e09671265b3398e4b4f3dc9a75782cff1c189437f768d26b48391eafc"
 
   url "https://github.com/evoja-brew/homebrew-picking-git-tools/releases/download/evj-hello/#{version.sub('.', '/')}/evj-hello-macos-universal.tar.gz"
   name "evj-hello"
