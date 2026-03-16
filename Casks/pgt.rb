fd4357830c219df66dbb312b9ddac7c5b63c3f79
chivorotkiv <git@chiv.info>
2026-03-16 02:35:16 +0000

    pgt v0.0.1

--- Casks/pgt.rb
+++ Casks/pgt.rb
@@ -1,13 +1,13 @@
 cask "pgt" do
-	version "0.0.0"
-	sha256 "86323456c4a1999cf3fa99e9884fa6addd3e3cf9f238348bbd3b31ce364ec2cc"
+  version "0.0.1"
+  sha256 "4cb8389e4323d606abfa05f7b154f39fb5516b8d3f17b6d672f7d66278d5333b"
 
-	url "https://github.com/evoja-brew/homebrew-tap/releases/download/pgt/#{version.sub('.', '/')}/pgt-macos-universal.tar.gz"
-	name "pgt"
-	desc "CLI tools for pgt"
-	homepage "https://github.com/evoja-brew/homebrew-tap"
+  url "https://github.com/evoja-brew/homebrew-tap/releases/download/pgt/#{version.sub('.', '/')}/pgt-macos-universal.tar.gz"
+  name "pgt"
+  desc "CLI tools for pgt"
+  homepage "https://github.com/evoja-brew/homebrew-tap"
 
-	binary "pgt"
-	binary "pgt-amend"
-	binary "pgt-log"
+    binary "pgt"
+  binary "pgt-amend"
+  binary "pgt-log"
 end
