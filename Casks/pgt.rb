62c242d9fa360eea1129086b0d5875d47d9e1723
chiv <git@chiv.info>
2026-03-16 03:27:31 +0100

    format code for consistency in pgt.rb KO-4

--- Casks/pgt.rb
+++ Casks/pgt.rb
@@ -1,12 +1,13 @@
 cask "pgt" do
-  version "0.0.0"
-  sha256 "86323456c4a1999cf3fa99e9884fa6addd3e3cf9f238348bbd3b31ce364ec2cc"
+	version "0.0.0"
+	sha256 "86323456c4a1999cf3fa99e9884fa6addd3e3cf9f238348bbd3b31ce364ec2cc"
 
-  url "https://github.com/evoja-brew/homebrew-tap/releases/download/pgt/#{version.sub('.', '/')}/pgt-macos-universal.tar.gz"
-  name "pgt"
-  desc "CLI tools for pgt"
-  homepage "https://github.com/evoja-brew/homebrew-tap"
+	url "https://github.com/evoja-brew/homebrew-tap/releases/download/pgt/#{version.sub('.', '/')}/pgt-macos-universal.tar.gz"
+	name "pgt"
+	desc "CLI tools for pgt"
+	homepage "https://github.com/evoja-brew/homebrew-tap"
 
-    binary "pgt"
-  binary "pgt-amend"
+	binary "pgt"
+	binary "pgt-amend"
+	binary "pgt-log"
 end
