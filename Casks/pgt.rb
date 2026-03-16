6855b067833087d9918a01344e2eb4167bdd7295
chivorotkiv <git@chiv.info>
2026-03-16 01:16:28 +0000

    pgt v0.0.0

--- Casks/pgt.rb
+++ Casks/pgt.rb
@@ -1,6 +1,6 @@
 cask "pgt" do
-  version "0.0.0-1"
-  sha256 "09e6e30acd7740f76066d442eaad4de46673fbe8df89ef05db389e8c45629de1"
+  version "0.0.0"
+  sha256 "86323456c4a1999cf3fa99e9884fa6addd3e3cf9f238348bbd3b31ce364ec2cc"
 
   url "https://github.com/evoja-brew/homebrew-picking-git-tools/releases/download/pgt/#{version.sub('.', '/')}/pgt-macos-universal.tar.gz"
   name "pgt"
